1b18f351744c67fc4ef257882efa96ad0698e410
columna1 <thecolumna@gmail.com>
2018-12-17 21:35:58 -0500

    Update readmap.lua

--- readmap.lua
+++ readmap.lua
@@ -1,1 +1,12 @@
-local map
+local map = {}
+local prefile = {}
+
+map.currentPos = 0
+
+function prefile.new()
+	local self = {}
+	setmetatable(self,{__index = m})
+	return self
+end
+
+return prefile
