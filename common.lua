f4e4bda2f54057167f5dbf98e6c0f140b65db39e
- <->
2019-10-27 13:06:38 -0500

    Create a common module

---
+++ common.lua
@@ -0,0 +1,13 @@
+--[[
+    Module: common
+    Common Values in this project.
+]]
+local common = {}
+
+-- Error formats.
+common.FMT_ERR_UNEXPECTED_KEY = "Unexpected key type: '%s' (%s)"
+
+-- Tostring formats for class objects.
+common.FMT_STR_VEC2 = "%s\t%s"
+
+return common
